7444b61d371630656fe464da8f24d095fadaa01b
Kirill Ivanov <bragov4ik@hotmail.com>
2022-05-22 17:51:26 +0300

    dynamic major in launch script

--- package/module_test/src/launch.sh
+++ package/module_test/src/launch.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 modprobe stack_device
 sleep 0.5
-mknod /dev/stack_file_kek_chrdev0 c 248 0
+
+module="stack_file_kek_chrdev"
+major=$(awk -v mod=$module '$2==mod{print $1}' /proc/devices)
+
+mknod /dev/stack_file_kek_chrdev0 c $major 0
 sleep 1
 test_program
-
